fbbf892f97b2e5057e99c17db48f55b656762a81
engy25 <engymohammed156@gmail.com>
2024-04-23 22:20:25 +0200

    first

--- app/Console/Commands/StartApp.php
+++ app/Console/Commands/StartApp.php
@@ -30,20 +30,20 @@ class StartApp extends Command
 
         // Run migrations
         Artisan::call('migrate');
-        $this->info('Migrations completed.');
+        $this->info('Migrations completed. ');
 
-        // Seed the database
+        // Seed the database and factory
         Artisan::call('db:seed');
         $this->info('Seeders completed.');
 
         // Start the development server in the background
-        $this->info('Start the development server.');
+        $this->info('Start the development server. ');
         shell_exec('php artisan serve --port=8000 --host=localhost --quiet &');
 
         $this->info('Application started successfully at http://localhost:8000');
 
         // Start the queue worker
-        $this->info('Starting queue worker.');
+        $this->info('Starting queue ');
         Artisan::call('queue:work', ['--daemon' => true]);
 
         $this->info('Queue worker started.');
